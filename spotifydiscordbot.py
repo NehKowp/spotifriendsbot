39154006527d6bde8384b699e9e44ec91ea3f83e
NehKowp <90217593+NehKowp@users.noreply.github.com>
2022-09-09 01:03:59 +0200

    new slash command

--- spotifydiscordbot.py
+++ spotifydiscordbot.py
@@ -1,14 +1,14 @@
-import json
+from typing import Optional
 import requests
 import discord
-from discord.ext import tasks
+from discord import app_commands
 import random as rand
-from collections import Counter
 import io
 import PIL.Image as PIL
+import json
 
 
-def stats(user1,user2):
+def statistics(user1,user2):
     r = requests.post('https://spotifriends-xfsi2jbksq-ew.a.run.app/name', json={
   "name": [user1,user2],
   "function": "s"
@@ -23,89 +23,105 @@ def titrecom(user1,user2):
 )
     return r.json()['data']
     
-def genres(user1):
+def genrestats(user1):
     r = requests.post('https://spotifriends-xfsi2jbksq-ew.a.run.app/name', json={"name": user1,"function": "g"})
     return r.json()
 
 
-dsclient = discord.Client()
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        
+        await self.tree.sync()
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+
+@client.tree.command()
+async def helpspotifriends(interaction: discord.Interaction):
+    """Help for SpotiFriends Bot!"""
+    embed=discord.Embed(title='Help for SpotiFriends Bot',color=0x21c452)
+    embed.add_field(name="**__ Statistiques :__**", value="`/stats user1 user2`", inline=False)
+    embed.add_field(name="**__ Titres en Communs :__**", value="`/common user1 user2`", inline=False)
+    embed.add_field(name="**__ Genres Préférés :__**", value="`/genres user1`", inline=False)
+
+    embed.set_footer(text="*SpotiFriends by Kow_p*")
+
+    await interaction.response.send_message(embed = embed)
+
+
+@client.tree.command()
+@app_commands.describe(
+    first_value='The link profile of the first user',
+    second_value='The link profile of the second user you want to compare',
+)
+async def stats(interaction: discord.Interaction, first_value: str, second_value: str):
+    """Compare the Spotify's stats of 2 users."""
+    await interaction.response.defer()
+    data = statistics(first_value,second_value)
+    pourcentage = data[0]
+    titrecommun = data[1]
+    embed=discord.Embed(title='Stats',color=0x21c452)
+    embed.add_field(name="**__Titres en Commun :__**", value="`"+str(titrecommun)+"`", inline=False)
+    embed.add_field(name="**__Pourcentage :__**", value="`"+str(pourcentage)+" %"+"`", inline=False)
+    embed.set_footer(text="*SpotiFriends by Kow_p*")
+    await interaction.followup.send(embed = embed)
+
+
+
+@client.tree.command()
+@app_commands.describe(
+    first_value='The link profile of the first user',
+    second_value='The link profile of the second user you want to compare',
+)
+async def common(interaction: discord.Interaction, first_value: str, second_value: str):
+    """Show Spotify's songs in common of the 2 users."""
+    await interaction.response.defer()
+    data = titrecom(first_value,second_value)
+    print(data)
+    embed2=discord.Embed(title='Song In Commons ',color=0x21c452)
+    i=0
+    for titre in data:
+            embed2.add_field(
+                name="**"+str(data[i][0])+"**",
+                value="`"+str(data[i][1])+"`",
+                inline=True
+                )
+            i+=1
+    embed2.set_image(url=data[rand.randint(0,(len(data)-1))][2])
+    await interaction.followup.send(embed = embed2)
+
+@client.tree.command()
+@app_commands.describe(
+    first_value='The link profile of the user')
+async def genres(interaction: discord.Interaction, first_value: str):
+    await interaction.response.defer()
+    """Show the different musics tastes and genres of the choosen user"""
+    resp = genrestats(first_value)
+    embed=discord.Embed(title='Genres',color=0x21c452)
+    embed.add_field(name="**__Genres préférés de __**", value="`"+first_value+"`", inline=False)
+    by = bytes(resp["data"])
+    buf = io.BytesIO(by)
+    image = PIL.open(buf)
+    image.save("./genrestats.png")
+    file = discord.File("./genrestats.png", filename="genrestats.png")
+    embed.set_image(url="attachment://genrestats.png")
+    embed.set_footer(text="*SpotiFriends by Kow_p*")
+    await interaction.followup.send(embed = embed, file=file)
+
 with open('./config.json', 'r') as f:
     config = json.load(f)
     dstoken = config['Discord Bot Token']
-    dschannel = int(config['Discord Channel ID'])
 
-@dsclient.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(dsclient))
-    channel = dsclient.get_channel(dschannel)
-    await channel.send("`"+"Connecté"+"`")
-
-@dsclient.event
-async def on_message(message):
-    if message.author == dsclient.user:
-        return
-
-    if message.content.startswith('!spotistats'):
-        msg = message.content
-        listpseudo=msg.split()
-        user1=listpseudo[1] 
-        user2=listpseudo[2]
-
-        data = stats(user1,user2)
-        pourcentage = data[0]
-        titrecommun = data[1]
-
-        embed=discord.Embed(color=0x21c452)
-        embed.add_field(name="**__Titres en Commun :__**", value="`"+str(titrecommun)+"`", inline=False)
-        embed.add_field(name="**__Pourcentage :__**", value="`"+str(pourcentage)+" %"+"`", inline=False)
-        embed.set_footer(text="*SpotiFriends by Kow_p*")
-        await message.channel.send(embed=embed)  
-
-    if message.content.startswith('!titrecommuns'):
-        msg = message.content
-        listpseudo=msg.split()
-        user1=listpseudo[1] 
-        user2=listpseudo[2]
-        data = titrecom(user1,user2)
-        embed2=discord.Embed(color=0x21c452)
-        i=0
-        for titre in data:
-                embed2.add_field(
-                    name="**"+str(data[i][0])+"**",
-                    value="`"+str(data[i][1])+"`",
-                    inline=True
-                    )
-                i+=1
-        embed2.set_image(url=data[rand.randint(0,(len(data)-1))][2])
-        await message.channel.send(embed=embed2) 
-
-    if message.content.startswith('!genrestats'):
-        msg = message.content
-        listpseudo=msg.split()
-        user1=listpseudo[1] 
-
-        await message.channel.send("`Chargement ...`") 
-        resp = genres(user1)
-
-        embed=discord.Embed(color=0x21c452)
-        embed.add_field(name="**__Genres préférés de __**", value="`"+user1+"`", inline=False)
-        by = bytes(resp["data"])
-        buf = io.BytesIO(by)
-        image = PIL.open(buf)
-        image.save("./genrestats.png")
-        file = discord.File("./genrestats.png", filename="genrestats.png")
-        embed.set_image(url="attachment://genrestats.png")
-        embed.set_footer(text="*SpotiFriends by Kow_p*")
-        await message.channel.send(embed=embed, file = file)  
-    
-    if message.content.startswith('!helpspotifriends'):
-        embed=discord.Embed(color=0x21c452)
-        embed.add_field(name="**__ Statistiques :__**", value="`!spotistats user1 user2`", inline=False)
-        embed.add_field(name="**__ Titres en Communs :__**", value="`!titrecommuns user1 user2`", inline=False)
-        embed.add_field(name="**__ Genres Préférés :__**", value="`!genrestats user1`", inline=False)
-
-        embed.set_footer(text="*SpotiFriends by Kow_p*")
-        await message.channel.send(embed=embed)  
-
-dsclient.run(dstoken)
-
+client.run(dstoken)
